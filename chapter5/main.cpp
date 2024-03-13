4fea3fbeb2685f59394ac93892acb948ea4996c6
DANG BA MINH <d.baminh@gmail.com>
2024-03-13 14:48:10 +0700

    feat: add chapter 5

---
+++ chapter5/main.cpp
@@ -0,0 +1,18 @@
+#include "../std_lib_facilities.h"
+
+class Bad_area {};
+int area(int length, int width) {
+    if (length <= 0 || width <= 0) throw Bad_area{};
+    return length * width;
+}
+
+int main() {
+    int l, w;
+    cin >> l >> w;
+    try {
+        cout << area(l, w) << "\n";
+    } catch (Bad_area) {
+        cout << "Oops! Bad arguments to area()\n";
+    }
+    return 0;
+}
