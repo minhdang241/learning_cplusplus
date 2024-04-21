be1dac893555737694072135ba82e73eb4a5ad1e
DANG BA MINH <d.baminh@gmail.com>
2024-04-21 11:17:57 +0700

    feat(chapter4,projects): update chapter 4 and projects

---
+++ chapter4/print_month.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    string abbreviation;
+    cin >> abbreviation;
+    unordered_map<string, string> map{
+            {"Jan" , "January"},
+            {"Feb" , "February"},
+            {"Mar" , "March"},
+            {"Apr" , "April"},
+            {"May" , "May"},
+            {"Jun" , "June"},
+            {"Jul" , "July"},
+            {"Aug" , "August"},
+            {"Sep" , "September"},
+            {"Oct" , "October"},
+            {"Nov" , "November"},
+            {"Dec" , "December"},
+    };
+
+    cout << map[abbreviation] << endl;
+    return 0;
+}
--- projects/rock_paper_scissors/main.cpp
+++ projects/rock_paper_scissors/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <set>
+
 using namespace std;
 
 int main() {
+    string input;
+    set<string> validInputs{"Rock", "Paper", "Scissors"};
     cout << "Welcome to Rock, Paper, Scissors!\n" << endl;
     cout << "Choose your move: Rock, Paper, or Scissors" << endl;
-    string input;
     cin >> input;
     cout << "> " << input << endl;
-    cout << "You wrote " << input << " to the console." << endl;
+    if (validInputs.find(input) == validInputs.end()) {
+        cout << "Invalid move. Please enter Rock, Paper, or Scissors." << endl;
+    } else {
+        cout << "You chose the " << input << ", great move." << endl;
+    }
 }
