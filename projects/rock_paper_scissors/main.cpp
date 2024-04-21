dcfc41692da4bf44286c6b9cc5819d4523e58a81
DANG BA MINH <d.baminh@gmail.com>
2024-04-21 11:35:46 +0700

    feat(projects): update projects

--- projects/rock_paper_scissors/main.cpp
+++ projects/rock_paper_scissors/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 #include <set>
+#include <random>
 
 using namespace std;
+string generateComputerChoice() {
+    vector<string> options{"Rock", "Paper", "Scissors"};
+    std::random_device rd;
+    std::mt19937  get(rd());
+    int randomChoice = get() % 3;
+    return options[randomChoice];
+}
 
+string compareChoices(string choice1, string choice2) {
+    if (choice1 == "Rock")  {
+        if (choice2 == "Paper") {
+            return "Paper covers Rock. Sorry, you lost.";
+        } else if (choice2 == "Scissors") {
+            return "Rock smashes Scissors. You win!";
+        } else {
+            return "It's a draw.";
+        }
+    } else if (choice1 == "Paper") {
+        if (choice2 == "Scissors") {
+            return "Scissors cut Paper. Sorry, you lost.";
+        } else if (choice2 == "Rock") {
+            return "Rock smashes Scissors. You win!";
+        } else {
+            return "It's a draw.";
+        }
+    } else {
+        if (choice2 == "Rock") {
+            return "Rock smashes Scissors. Sorry, you lost.";
+        } else if (choice2 == "Paper") {
+            return "Scissors cut Paper. You win!";
+        } else {
+            return "It's a draw.";
+        }
+    }
+}
 int main() {
     string input;
     set<string> validInputs{"Rock", "Paper", "Scissors"};
@@ -13,6 +48,8 @@ int main() {
     if (validInputs.find(input) == validInputs.end()) {
         cout << "Invalid move. Please enter Rock, Paper, or Scissors." << endl;
     } else {
-        cout << "You chose the " << input << ", great move." << endl;
+        string computerChoice = generateComputerChoice();
+        string result = compareChoices(input, computerChoice);
+        cout << "The computer chose " << computerChoice << ". " << result << endl;
     }
 }
