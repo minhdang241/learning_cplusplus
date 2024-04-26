d6ed6af09917e861ae688250aaf22ab506e21028
DANG BA MINH <d.baminh@gmail.com>
2024-04-26 15:27:46 +0700

    feat: update chapter 17

--- chapter17/main.cpp
+++ chapter17/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-using namespace std;
+int main(){
 
+    int arr[] = {1, 2, 3, 4, 5};
 
-int main() {
-    int num = 10;
-    int* ptr = &num;
-    cout << "Original value: " << num << endl;
-    cin >> *ptr;
-    cout << "Modified value: " << num << endl;
+    int size = sizeof(arr);
+    std::cout << "Size of the array: " << size << std::endl; // Output: 20
+
+    int length = sizeof(arr) / sizeof(arr[0]);
+    std::cout << "Length of the array: " << length << std::endl; // Output: 5
     return 0;
 }
