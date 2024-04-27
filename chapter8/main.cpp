e206c15c0120bde3cc33a022445cff2b55046a4f
DANG BA MINH <d.baminh@gmail.com>
2024-04-27 10:11:59 +0700

    feat: finalize the zoo project

--- chapter8/main.cpp
+++ chapter8/main.cpp
@@ -1,6 +1,16 @@
 #include "../std_lib_facilities.h"
+
 int main() {
-    extern int i;
-    extern int i;
-    cout << i << endl;
+    vector<int> vi(10);
+    vector<int> &vi_ref = vi;
+    int i = 0;
+    int &i_ref = i;  // This is the reference to i
+    char a[] = "123";
+    char (&a_ref)[4] = a;
+
+    for (auto v: vi) {
+        cout << v << endl;
+    }
+
+    cout << "hello\n";
 }
