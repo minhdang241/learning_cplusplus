9b4f791b246e983abc54985c3e82af48c8962ce3
DANG BA MINH <d.baminh@gmail.com>
2024-04-30 11:01:20 +0700

    feat: add chapter 19.2

---
+++ learningcpp/chapter19/19.1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by DANG BA MINH on 29/04/2024.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int* ptr{new int};
+    *ptr = 7;
+    cout << *ptr << endl;
+    return 0;
+}
