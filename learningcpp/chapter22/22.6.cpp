b5805808377883ae6cb8945f55c2757018e3491b
minh.dang4 <minh.dang4@mservice.com.vn>
2024-05-02 10:01:47 +0700

    feat: add chapter 22.6

---
+++ learningcpp/chapter22/22.6.cpp
@@ -0,0 +1,36 @@
+//
+// Created by DANG BA MINH on 01/05/2024.
+//
+#include <iostream>
+
+using namespace std;
+
+class Resource {
+private:
+    int m_cores;
+public:
+    Resource() {
+        cout << "Resource acquired\n";
+    }
+
+    Resource(int cores) : m_cores{cores} {
+        cout << "Resource acquired with " << m_cores << " core\n";
+    }
+
+    ~Resource() {
+        cout << "Resource destroyed\n";
+    }
+};
+
+int main() {
+//    Resource *res{new Resource{}};
+//    shared_ptr<Resource> ptr1{res};
+    auto ptr1 = make_shared<Resource>(10);
+    {
+//        shared_ptr<Resource> ptr2{ptr1};
+        auto ptr2{ptr1};
+        cout << "Killing one shared pointer\n";
+    }
+    cout << "Killing another shared pointer\n";
+    return 0;
+}
