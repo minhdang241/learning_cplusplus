00ef42d1e55cd914593196ff30ac65c07556a21c
DANG BA MINH <d.baminh@gmail.com>
2025-09-03 15:07:22 +1000

    feat: function template

--- primer/chapter16/src/function_template.cpp
+++ primer/chapter16/src/function_template.cpp
@@ -3,11 +3,27 @@
 #include <string>
 
 int main(void) {
+    struct sale_data {
+        int val = 0;
+        auto operator<(const sale_data &rhs) const -> bool {
+            return this->val < rhs.val;
+        }
+    };
     // std::cout << compare<double>(2.0, 1.0) << "\n";
     // std::cout << compare<int>(2, 2) << "\n";
     // std::cout << compare<std::string>("a", "ac") << "\n";
-    std::cout << compare<3,4>("hi", "mom") << "\n";
+    std::cout << compare<sale_data>({1}, {2}) << "\n";
+    // std::cout << compare<3,4>("hi", "mom") << "\n";
     auto int_5a = StaticArray<int, 5>();
     std::cout << int_5a.size() << "\n";
+    auto v = std::vector<int>{1,2,3};
+    // auto val = 3;
+    auto it = my_find(v.begin(), v.end(), 3);
+    std::cout << *it << "\n";
+    double l[] = {1.2, 2, 3};
+    print(l);
+    std::string sarr[] = {"alpha", "beta", "gamma"};
+    print(sarr);
+    std::cout << size(v) << "\n";
     return 0;
 }
--- primer/chapter16/src/function_template.hpp
+++ primer/chapter16/src/function_template.hpp
@@ -2,11 +2,13 @@
 #define FUNCTION_TEMPLATE
 
 #include <cstring>
+#include <functional>
+#include <iostream>
 
 template <typename T>
 int compare(const T &v1, const T &v2) {
     if (v1 < v2) return -1;
-    if (v1 > v2) return 1;
+    if (v2 < v1) return -1;
     return 0;
 }
 
@@ -25,5 +27,30 @@ class StaticArray {
 
     auto size() const -> std::size_t { return sz; }
 };
+
+template <typename T, typename V>
+auto my_find(T it1, T it2, const V &value) -> T {
+    while (it1 != it2) {
+        if (*it1 == value) {
+            return it1;
+        }
+        ++it1;
+    }
+    return it1;
+}
+
+template <typename T, std::size_t sz>
+auto print(const T (&p)[sz]) -> void {
+    for (auto i = 0ul; i < sz; ++i) {
+        std::cout << p[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+template<typename T, std::size_t sz>
+inline constexpr auto size() -> std::size_t {
+    return sz;
+}
+
 
 #endif
